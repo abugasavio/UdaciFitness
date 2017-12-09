d126943674ab67f01f9f157bab5bc18035e92ae2
Savio Abuga <savioabuga@gmail.com>
2017-12-09 12:27:20 +0300

    add date header

--- components/DateHeader.js
+++ components/DateHeader.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import { Text } from 'react-native';
 
-const DateHeader = ({date}) => {
-	return (
-		<Text>
-			{date}
-		</Text>
-	);
+const DateHeader = ({ date }) => {
+  return <Text>{date}</Text>;
 };
 
 export default DateHeader;

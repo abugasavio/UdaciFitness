8f13ba6f66065189cecdccc8b23196acda47b642
Savio Abuga <savioabuga@gmail.com>
2017-12-30 16:03:37 +0300

    finish styles and work on history page

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { View, StyleSheet } from 'react-native';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import AddEntry from './components/AddEntry'
+import History from './components/History';
 import reducer from './reducers';
 
 export default class App extends React.Component {
@@ -10,7 +11,7 @@ export default class App extends React.Component {
     return (
       <Provider store={createStore(reducer)}>
       <View style={ styles.container}>
-        <AddEntry />
+        <History />
       </View>
       </Provider>
     );

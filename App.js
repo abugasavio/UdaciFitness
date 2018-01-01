6c5c892a5383619d05527134dddef8f6d3280ada
Savio Abuga <savioabuga@gmail.com>
2018-01-01 12:08:58 +0300

    finished styles

--- App.js
+++ App.js
@@ -1,29 +1,20 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
+import React from 'react'
+import { View } from 'react-native'
 import AddEntry from './components/AddEntry'
-import History from './components/History';
-import reducer from './reducers';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducers'
+import History from './components/History'
 
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={createStore(reducer)}>
-      <View style={ styles.container}>
-        <History />
-      </View>
+        <View style={{flex: 1}}>
+          <History />
+        </View>
       </Provider>
-    );
+    )
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginLeft: 10,
-    marginRight: 10,
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-})
